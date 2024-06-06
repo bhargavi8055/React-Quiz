dea6fd5abbbaaee9e6bedfbdb5d75668ab3c3557
bhargavi8055 <59086546+bhargavi8055@users.noreply.github.com>
2024-06-06 19:43:45 +0530

    Update quiz.js

--- src/contexts/quiz.js
+++ src/contexts/quiz.js
@@ -1,30 +1,65 @@
-import { createContext,useReducer } from "react";
-import questions from '../data';
-import { shuffleAnswers } from "../helpers";
+import { act, createContext,useReducer } from "react";
+import { shuffleAnswers,normalizeQuestions } from "../helpers";
 const initialState ={
     currentQuestionIndex:0,
-    questions:questions,
-    // questions
+    questions:[],
     showResults:false,
-    answers:shuffleAnswers(questions[0]),
+    answers:[],
+    currentAnswer:"",
+    correctAnswerCount:0,
+    error:null
 };
 
 const reducer = (state,action)=>{
-    if(action.type==='NEXT_QUESTION'){
-        const showResults =state.currentQuestionIndex ===state.questions.length-1;
-        const currentQuestionIndex  = showResults ?state.currentQuestionIndex:state.currentQuestionIndex+1;
-        const answers = showResults?[]:shuffleAnswers(state.questions[currentQuestionIndex]);
-        return {
-            ...state,
-            currentQuestionIndex,
-            showResults,
-            answers
-        };
+    switch(action.type){
+        case "SELECT_ANSWER":
+            const correctAnswerCount = 
+            action.payload === state.questions[state.currentQuestionIndex].correctAnswer 
+            ? state.correctAnswerCount+1
+            :state.correctAnswerCount
+            return {
+                ...state,
+                currentAnswer:action.payload,
+                correctAnswerCount
+            }
+            
+        
+        case "NEXT_QUESTION":
+            const showResults =state.currentQuestionIndex ===state.questions.length-1;
+            const currentQuestionIndex  = showResults ?state.currentQuestionIndex:state.currentQuestionIndex+1;
+            const answers = showResults?[]:shuffleAnswers(state.questions[currentQuestionIndex]);
+            return {
+                ...state,
+                currentQuestionIndex,
+                showResults,
+                answers,
+                currentAnswer:""
+            };
+           
+        
+        case "RESTART":
+            return initialState;
+
+        
+        case "LOADED_QUESTIONS":
+            const normalizedQuestions = normalizeQuestions(action.payload);
+            return {
+                ...state,
+                questions:normalizedQuestions,
+                answers:shuffleAnswers(normalizedQuestions[0])
+            };
+        
+        case "SERVER_ERROR":
+            return {
+                ...state,
+                error:action.payload
+            }
+            
+        default:
+            return state;
+        
     }
-    if(action.type==="RESTART"){
-        return initialState;
-    }
-    return state;
+    
 }
 
 // create a context
@@ -33,6 +68,5 @@ export const QuizContext = createContext();
 // create a provider which will give data to all components
 export const QuizProvider = ({children})=>{
     const stateValue = useReducer(reducer,initialState);
-    console.log("state",stateValue)
     return <QuizContext.Provider value={stateValue} >{children}</QuizContext.Provider>
 }
