c949a5b55644b997840f16495c686708bf601861
bhargavi8055 <59086546+bhargavi8055@users.noreply.github.com>
2024-06-06 19:44:40 +0530

    Update Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,22 +1,25 @@
-import { useContext } from "react";
 import Answer from "./Answer";
+import { useContext } from "react";
 import { QuizContext } from "../contexts/quiz";
 
-const Question= ({questions})=>{
-    const [quizState] = useContext(QuizContext);
-    console.log("quizState in Question Comp",quizState)
-    const currentQuestion = quizState.questions[quizState.currentQuestionIndex].question;
-    // console.log("currentQuestion",currentQuestion)
+const Question= ()=>{
+    const [quizState,dispatch] = useContext(QuizContext);
+    const currentQuestion = quizState.questions[quizState.currentQuestionIndex];
     return (
         <div>
-            <div className="question">{currentQuestion}</div>
+            <div className="question">{currentQuestion.question}</div>
             <div className="answers">
                 {quizState.answers.map((answer,index)=>(
-                    <Answer answerText={answer} key={index}/>
+                    <Answer answerText={answer}
+                     key={index} 
+                     index={index}
+                     currentAnswer={quizState.currentAnswer}
+                     correctAnswer={currentQuestion.correctAnswer}
+                     onSelectAnswer={(answerText)=>dispatch({type:"SELECT_ANSWER",payload:answerText})} />
                 ))}
             </div>
         </div>
-    )
+    );
 }
 
 export default Question;
